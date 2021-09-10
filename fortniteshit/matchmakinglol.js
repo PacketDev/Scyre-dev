bda23fe91afe6e396097abec96c951c10b20dcbb
zinx <63958372+zinx-YT@users.noreply.github.com>
2021-09-10 19:39:15 +0100

    Update matchmakinglol.js

--- fortniteshit/matchmakinglol.js
+++ fortniteshit/matchmakinglol.js
@@ -1,19 +1,4 @@
 module.exports = (express) => {
-	/*
-  express.get(
-	'/fortnite/api/game/v2/matchmakingservice/ticket/player/*',
-	async (req, res) => {
-		res.json({
-			serviceUrl: 'wss://Scyre-dev.zinx28.repl.co',
-			ticketType: 'mms-player',
-			payload: '69=',
-			signature: '420='
-		});
-		res.status(200);
-		res.end();
-	}
-);
-*/
 express.get(
 	'/fortnite/api/game/v2/matchmaking/account/:accountId/session/:sessionId',
 	async (req, res) => {
